ae63de97b0d7c61816aea71be425e4390d9f7ff8
Leonardo Omoto Sato <omotosa@uwindsor.ca>
2026-07-19 00:00:58 -0300

    Chart creation student table configuration

--- src/main/java/com/map/seatingmapgenerator/controller/ChartCreation.java
+++ src/main/java/com/map/seatingmapgenerator/controller/ChartCreation.java
@@ -1,6 +1,7 @@
 package com.map.seatingmapgenerator.controller;
 
 import com.map.seatingmapgenerator.model.Classroom;
+import com.map.seatingmapgenerator.model.Grade;
 import com.map.seatingmapgenerator.model.Student;
 import com.map.seatingmapgenerator.service.ClassroomService;
 import com.map.seatingmapgenerator.service.SeatingService;
@@ -10,9 +11,16 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ChartCreation {
 
@@ -23,27 +31,49 @@ public class ChartCreation {
 
         //initializing student table
         configureStudentTable();
-        loadStudents();
 
         //initializing classroom table
         configureClassroomTable();
-        loadClassrooms();
     }
 
-    private final ObservableList<Student> students = FXCollections.observableArrayList();
+    @FXML
+    private TreeTableView<Object> studentTable;
+    @FXML
+    private TreeTableColumn<Student, CheckBox> selectedStudentColumn;
+    @FXML
+    private TreeTableColumn<Classroom, Integer> studentIdColumn;
+    @FXML
+    private TreeTableColumn<Classroom, String> nameColumn;
 
-    private void loadStudents() {
-        students.setAll(seatingService.getStudents());
-        students.sort(Comparator.comparing(Student::getGrade).thenComparing(Student::getName));
+    @FXML
+    private void configureStudentTable() {
+
+        TreeItem<Object> root = new TreeItem<>("Students");
+        root.setExpanded(true);
+
+        //Group students by grade
+        Map<Grade, List<Student>> studentsByGrade =
+                seatingService.getStudents().stream()
+                        .collect(Collectors.groupingBy(Student::getGrade));
+
+        //Create grade nodes
+        for (Map.Entry<Grade, List<Student>> entry : studentsByGrade.entrySet()) {
+
+            TreeItem<Object> gradeNode =
+                    new TreeItem<>("Grade " + entry.getKey());
+
+            // Add students under the grade
+            for (Student student : entry.getValue()) {
+                gradeNode.getChildren()
+                        .add(new TreeItem<>(student));
+            }
+
+            root.getChildren().add(gradeNode);
+        }
+
+        studentTable.setRoot(root);
+        studentTable.setShowRoot(false);
     }
-
-    private final ObservableList<Classroom> classrooms = FXCollections.observableArrayList();
-
-    private void loadClassrooms() {
-        classrooms.setAll(seatingService.getClassrooms());
-        classrooms.sort(Comparator.comparing(Classroom::getId));
-    }
-
 
     @FXML
     private void goBack(ActionEvent event) throws IOException {
